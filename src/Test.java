75c254a5d27e9ca4b8c38d0321089e1ee13fad71
jokerdavid <58126662+jokerdavid@users.noreply.github.com>
2019-11-26 11:17:19 +0800

    Update Test.java

--- src/Test.java
+++ src/Test.java
@@ -8,13 +8,14 @@ public class Test  {
 
     private String str = "Hello World !!!" ;
     private String strGo = "重启程序员之路，Go Go Go！Fighting！！！" ;
+    private String strFighting = "干就完了" ;
     public static void main(String[] args) {
         Test t = new Test() ;
         System.out.print(t.getStr()) ;
 
     }
     public String getStr(){
-        return this.str +"\n"+this.strGo ;
+        return this.str +"\n"+this.strGo + "\n" + this.strFighting ;
     }
 
 
